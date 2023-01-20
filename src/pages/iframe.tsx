aeeb029cd4470e96cd28d3e9d843ccab51abada6
Filip Masar <filip.masar99@gmail.com>
2023-01-20 08:33:22 +0100

    some updates

--- src/pages/iframe.tsx
+++ src/pages/iframe.tsx
@@ -1,14 +1,11 @@
 import { FC } from "react"
-import Script from "next/script"
-
 
 const Iframe: FC = () => {
-
 
   return (
     <div>
       <h1>SCUTI IFRAME</h1>
-      <iframe src="http://localhost:16108/" style={{ height: "800px", width: "600px" }}></iframe>
+      <iframe src="http://localhost:3000/" style={{ height: "800px", width: "600px" }}></iframe>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,50 @@
-import { FC } from "react"
+import { FC, useCallback, useState } from "react"
 import Script from "next/script"
+import Link from "next/link"
 
 
 const Scuti: FC = () => {
+  const [scutiLoaded, setScutiLoaded] = useState(false)
+  const [scutiOpened, setScutiOpened] = useState(false)
 
-
-  const onScutiLoaded = () => {
-    console.log("SCUTI LOADED");
-    console.log("SCUTI LOADED");
-    console.log("SCUTI LOADED");
+  const onScutiLoaded = useCallback(() => {
     console.log("SCUTI LOADED");
     (window as any).SCUTI_SDK.initialize("65407cb8-cd30-48e5-90cf-3275486aba0e")
+
+    setScutiLoaded(true)
+  }, [])
+
+  const onBackToGame = useCallback(() => {
+    console.log("on back to game")
+    setScutiOpened(false)
+  }, [])
+
+  const onScutiExchange = useCallback(() => {
+    console.log("onScutiExchange")
+  }, [])
+
+  const showScuti = useCallback(() => {
+    if (!scutiLoaded || scutiOpened) return
+
+    console.log("SHOW scuti");
+    setScutiOpened(true);
+    
     setTimeout(() => {
-      (window as any).SCUTI_SDK.renderStore("scuti-store", { onBackToGame, onScutiExchange }, { width: "100%", height: "100%" })
+      (window as any).SCUTI_SDK.renderStore(
+        "scuti-store", 
+        { onBackToGame, onScutiExchange }, 
+        { width: "100%", height: "100%" }
+      )
     }, 1000)
-  }
-
-  
-  const onBackToGame = () => {
-    console.log("on backto game")
-  }
-
-  const onScutiExchange = () => {
-    console.log("test if now work when i add both functions")
-  }
+  }, [onBackToGame, onScutiExchange, scutiLoaded, scutiOpened])
 
   return (
     <div>
       <Script src="https://staging.scuti-sdk.js.run.app.scuti.store/browser.js" onLoad={onScutiLoaded} />
-      <h1>SCUTI</h1>
-      <div className="fixed top-0 left-0 h-screen w-full bg-main-bk z-[50000] flex flex-col">
-        <div id="scuti-store" style={{ height: "800px" }} />
-      </div>
+      <button onClick={showScuti} style={{marginRight: 16}}>SHOW SCUTI</button>
+      <Link href="/iframe">open this page in iframe</Link>
+
+      {scutiOpened && <div id="scuti-store" style={{ height: "800px" }} />}
     </div>
   )
 }
